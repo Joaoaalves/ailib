a69897bdb8461b9c3d2e9f5d8d6a47937676de93
Joaoaalves <joaoaa21@hotmail.com>
2024-09-11 16:02:33 -0300

    feat(Collection Creation): Created submit button on Collection creation form

--- renderer/components/Collection/CreateCollectionForm.tsx
+++ renderer/components/Collection/CreateCollectionForm.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Input from "../ui/Input";
 import { useCollections } from "@/hooks/use-collections";
+import { Button } from "../ui/Button";
 
 export default function CreateCollectionForm({ setOpen }) {
     const { createCollection } = useCollections();
@@ -15,13 +16,15 @@ export default function CreateCollectionForm({ setOpen }) {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="space-y-6 mt-6">
             <Input
                 type="text"
                 placeholder="Collection name..."
                 value={collectionName}
                 onChange={(e) => setCollectionName(e.target.value)}
+                required
             />
+            <Button className="w-full">Create Collection</Button>
         </form>
     );
 }
