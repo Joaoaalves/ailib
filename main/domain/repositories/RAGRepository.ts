bdf8e1c51ceb30a750f1e8f3d7e0c14be1cf366f
Joaoaalves <joaoaa21@hotmail.com>
2024-11-04 17:29:48 -0300

    chore: removed unused repositorie

--- main/domain/repositories/RAGRepository.ts
+++
@@ -1,9 +0,0 @@
-export default interface IRAGRepository {
-    summarizePages(
-        pages: string[],
-        lastSummary: string,
-        model: string,
-    ): Promise<string | null>;
-    createHypotheticalDocument(query: string, model: string): Promise<string>;
-    generateQueries(query: string, model: string): Promise<string>;
-}
